9152f24ea0b0fe28cd74753096855ea65bdfd73e
Tsebang Maduna <tsebangmaduna6@gmail.com>
2026-05-30 00:37:01 +0200

    fixed back to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,33 @@
-console.log("Prime Synaptum website loaded.");
+// Back To Top Button
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+  if(window.scrollY > 300){
+
+    backToTop.style.display = "block";
+
+  } else {
+
+    backToTop.style.display = "none";
+
+  }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+  window.scrollTo({
+    top:0,
+    behavior:"smooth"
+  });
+
+});
+
+// AOS Animation Init
+
+AOS.init({
+  duration:1000,
+  once:true
+});
